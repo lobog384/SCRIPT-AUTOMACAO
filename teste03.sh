b70cd28a995fd577e4fda3041f5898996f902bfb
aluno <aluno@Lab-08>
2020-03-05 10:40:12 -0300

    denovo

--- teste03.sh
+++ teste03.sh
@@ -8,13 +8,19 @@
 cd imagens-livros
 
 
+
+echo 'convertendo as imagens'
+
 for imagem in *.jpg
 do
 
 	echo $imagem
 	img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
+	
 	echo $img_sem_ext
 	convert $imagem $img_sem_ext.png
+
+	echo 'imagens convertidas, fim!'
 done
 
 cd ..
