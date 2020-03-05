ee1ac8950341cc2bbfc1506eb512c06508d0b54e
aluno <aluno@Lab-08>
2020-03-05 10:21:26 -0300

    Aula 05/03

---
+++ teste03.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#Convertendo imagem
+#
+#Gustavo lobo
+
+
+cd imagens-livros
+
+
+for imagem in *.jpg
+do
+
+	echo $imagem
+	img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
+	echo $img_sem_ext
+	convert $imagem $img_sem_ext.png
+done
+
+cd ..
